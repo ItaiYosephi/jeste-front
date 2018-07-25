4bbc6bfccfd27651ea6480a6fac276283d7d8767
Shlomi <Shlomixg@gmail.com>
2018-07-25 19:34:22 +0300

    basic styilng of jestes card

--- src/components/jestes/JestePreview.vue
+++ src/components/jestes/JestePreview.vue
@@ -1,23 +1,66 @@
 <template>
     <section class="jeste-preview">
-        jeste-preview
-        {{jeste}}
+        <img :src="jeste.imgs_url[0]" />
+        <div class="text">
+            {{resUser.firstName}} {{resUser.lastName}}, {{resUser.Age}}
+            <br/>
+            Destination: {{jeste.destination_loc}}
+            <br/>
+            Starting from:
+            <br/>
+            {{jeste.time_frame.from | dateFormat}}
+            <br/>
+            Deadline:
+            <br/>
+            {{jeste.time_frame.until | dateFormat}}
+            <br/>
+            {{jeste.description}}
+            <br/>
+            Tags: <span v-for="keyword in jeste.keywords">{{keyword}}, </span>
+        </div>
     </section>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
-    name: 'jestePreview',
-    props: {
-        jeste: Object
+  name: "jestePreview",
+  props: {
+    jeste: Object
+  },
+  computed: {
+    resUser() {
+      return this.jeste.res_user.details;
     },
-}
+    reqUser() {
+      return this.jeste.req_user.details;
+    }
+  },
+  filters: {
+      dateFormat(timestamp) {
+          console.log(timestamp);
+          return moment(+timestamp).fromNow() + ', (' + moment(+timestamp).format('HH:mm:ss, DD/MM/YYYY') + ')';
+      }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/styles/_vars.scss';
+@import "../../assets/styles/_vars.scss";
 
 .jeste-preview {
-    color: $bgColor;
+  padding: 0;
+  color: $mainColor;
+}
+
+.jeste-preview.text {
+    padding: 5px;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
 }
 </style>
