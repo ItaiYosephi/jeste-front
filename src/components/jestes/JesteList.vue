5ba645841b5784f4147fcc4ad29ac72c258a0507
Shlomi <Shlomixg@gmail.com>
2018-07-26 19:05:01 +0300

    cards layout

--- src/components/jestes/JesteList.vue
+++ src/components/jestes/JesteList.vue
@@ -1,15 +1,17 @@
 <template>
-
-    <ul class="jeste-list clean-list flex">
-		<v-layout>
-            <router-link tag="li"
-                        v-for="jeste in jestes" 
-                        :key="jeste._id"
-                        :to="'/jeste/' + jeste._id">
-                <JestePreview :jeste="jeste" />
-            </router-link>
-		</v-layout>
-    </ul>
+    <v-layout>
+        <JestePreview
+            v-for="jeste in jestes"
+            :key="jeste._id"
+            :to="'/jeste/' + jeste._id"
+            :jeste="jeste" />
+    </v-layout>
+     <!-- <v-flex ma-2
+            v-for="jeste in jestes" 
+            :key="jeste._id"
+            :to="'/jeste/' + jeste._id">
+            <JestePreview :jeste="jeste" />
+        </v-flex> -->
 </template>
 
 <script>
@@ -29,6 +31,11 @@ export default {
 <style lang="scss" scoped>
 @import '../../assets/styles/_vars.scss';
 @import '../../assets/styles/_helpers.scss';
+
+.wrapper {
+    // flex-wrap: wrap;
+    padding: 10px;
+}
 
 // .jeste-list > li {
 //     width: 300px;
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -11,7 +11,7 @@
 			<v-btn type="submit" :disabled="!valid">Login</v-btn>
 
         </v-form>
-			<button @click="logout">Logout</button>
+			<v-btn @click="logout">Logout</v-btn>
         
 
     </section>
