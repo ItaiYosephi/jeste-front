931f1994728ce3bb21e1386a847337184a1e579d
Itai Yosephi <itai.yosephi@gmail.com>
2018-07-30 16:07:22 +0300

    minor changes

--- src/components/jestes/JestePreview.vue
+++ src/components/jestes/JestePreview.vue
@@ -1,51 +1,102 @@
 <template>
-		<v-flex xl3 class="jeste-card-wrapper">
-        <v-card class="jeste-card" tile hover :to="'/jeste/' + jeste._id" height="100%">
-            <v-card-media :src="jeste.imgs_url[0]" height="200px"></v-card-media>
-			<v-card-title primary-title>
-				<div>
-					<h3 class="headline mb-0">{{jeste.title}}</h3>
-					<div>{{jeste.description}}</div>
-				</div>
-			</v-card-title>
-			<v-card-actions>
-				<v-btn flat color="blue" @click.prevent="">details</v-btn>
-				<v-btn flat color="blue" @click.prevent="">Jeste It!</v-btn>
-			</v-card-actions>
-		</v-card>
-    </v-flex>
+  <v-flex class="jeste-card-wrapper" xs12 sm4 md3>
+    <v-card class="jeste-card" tile hover :to="'/jeste/' + jeste._id" height="400px">
+      <v-card-media :src="jeste.imgs_url[0]" height="200px"></v-card-media>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{jeste.title}}</h3>
+          <span class="grey--text">{{jeste.formatted_address}}</span>
+          <div class="block-with-text" ref="desc">{{jeste.description}}</div>
+        </div>
+      </v-card-title>
+      <v-card-actions class="actions">
+        <v-btn flat color="blue" @click.prevent="" >details</v-btn>
+        <v-btn flat color="blue" @click.prevent="">Jeste It!</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-import moment from "moment";
+import moment from 'moment';
 
 export default {
-  name: "jestePreview",
-  props: {
-    jeste: Object
+	name: 'jestePreview',
+	props: {
+		jeste: Object
   },
-  computed: {
-    reqUser() {
-      return this.jeste.req_user.details;
-    }
+  mounted() {
+    console.log(this.$refs.desc.scrollHeight);
+    console.log(this.$refs.desc.offsetHeight);
+    
+
   },
-  filters: {
-    dateFormat(timestamp) {
-      return (
-        moment(+timestamp).fromNow() +
-        ", (" +
-        moment(+timestamp).format("HH:mm:ss, DD/MM/YYYY") +
-        ")"
-      );
-    }
-  }
+	computed: {
+		reqUser() {
+			return this.jeste.req_user.details;
+		}
+	},
+	filters: {
+		dateFormat(timestamp) {
+			return (
+				moment(+timestamp).fromNow() +
+				', (' +
+				moment(+timestamp).format('HH:mm:ss, DD/MM/YYYY') +
+				')'
+			);
+		}
+	}
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/styles/_vars.scss";
+@import '../../assets/styles/_vars.scss';
 
 .jeste-card-wrapper {
-  max-width: 400px;
+	max-width: 400px;
 }
+.jeste-card-wrapper .description {
+  //  white-space: no-wrap; 
+  //   overflow: hidden;
+  //   text-overflow: ellipsis;
+  //   height: 150px;
+  
+}
+@mixin multiLineEllipsis($lineHeight: 1.2em, $lineCount: 1, $bgColor: white){
+  overflow: hidden;
+  position: relative;
+  line-height: $lineHeight;
+  max-height: $lineHeight * $lineCount; 
+  text-align: justify;
+  margin-right: -1em;
+  padding-right: 1em;
+  &:before {
+    content: '...';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    width: 1em;
+    height: 1em;
+    margin-top: 0.2em;
+    background: $bgColor;
+  }
+}
+
+.block-with-text {
+  @include multiLineEllipsis($lineHeight: 1.2em, $lineCount: 5, $bgColor: white);  
+  }
+  
+  .jeste-card {
+	flex-direction: column;
+	display: flex;
+}
+.actions {
+  margin-top: auto;
+}
+
 </style>
