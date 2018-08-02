c8a8d056036d0cafc11d3e7c674699dd34b77452
Itai Yosephi <itai.yosephi@gmail.com>
2018-08-02 14:24:25 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store';
 import './registerServiceWorker';
 import axios from 'axios';
 import Vuetify from 'vuetify';
-import Chat from 'vue-beautiful-chat'
+import Chat from 'vue-beautiful-chat';
 import 'vuetify/dist/vuetify.min.css';
 import * as VueGoogleMaps from 'vue2-google-maps';
 const GOOGLE_API_KEY = 'AIzaSyB1XEp2JKq8CAO8EbBSDGEvjrVC264DLPA';
@@ -13,33 +13,31 @@ import VueSocketio from 'vue-socket.io';
 import io from 'socket.io-client';
 // Vue.use(VueSocketio, 'http://localhost:3000');
 // io('http://localhost:3000')
-const URL = (process.env.PORT)? '/' : '//localhost:3000';
-
-Vue.use(VueSocketio, io(URL), store);
+const SOCKET_URL =
+	process.env.NODE_ENV !== 'development' ? '/' : '//localhost:3000';
+Vue.use(VueSocketio, io(SOCKET_URL), store);
 
 Vue.use(VueGoogleMaps, {
 	load: {
 		key: GOOGLE_API_KEY,
 		libraries: 'places' // This is required if you use the Autocomplete plugin
 	}
-	
 });
 
 Vue.use(Vuetify, {
 	theme: {
-		primary: "#039BE5",
-		secondary: "#031424",
-		accent: "#424242",
+		primary: '#039BE5',
+		secondary: '#031424',
+		accent: '#424242',
 		text: '#fff',
-		bg: "#e9e9e9",
-		error: "#f44336",
-		warning: "#ffeb3b",
-		info: "#2196f3",
-		success: "#4caf50"
+		bg: '#e9e9e9',
+		error: '#f44336',
+		warning: '#ffeb3b',
+		info: '#2196f3',
+		success: '#4caf50'
 	}
-})
-Vue.use(Chat)
-
+});
+Vue.use(Chat);
 
 axios.defaults.withCredentials = true;
 Vue.config.productionTip = false;
