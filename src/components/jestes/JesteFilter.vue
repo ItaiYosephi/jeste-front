61312883278a38436287d814f28189424219f620
Itai Yosephi <itai.yosephi@gmail.com>
2018-08-04 19:50:39 +0300

    aa

--- src/components/jestes/JesteFilter.vue
+++ src/components/jestes/JesteFilter.vue
@@ -1,6 +1,5 @@
 <template>
 	<v-form>
-		{{filter}}
 		<v-layout row wrap>
 			<v-flex xs12 md7>
 				<v-text-field color="secondary" v-model="txt" flat hide-details clearable prepend-inner-icon="search" label="Free Text" @keyup.enter.native="filterJestes" @click:prepend-inner="filterJestes">
